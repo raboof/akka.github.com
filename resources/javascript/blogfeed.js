778cde775f2e7e618d79e87b41d7ce0bbf1d0cf3
Konrad Malawski <konrad.malawski@project13.pl>
2016-07-06 16:23:35 +0200

    fix rss include

--- resources/javascript/blogfeed.js
+++ resources/javascript/blogfeed.js
@@ -1,4 +1,4 @@
-$("#blog-feed").rss("http://letitcrash.com/rss", {
+$("#blog-feed").rss("http://blog.akka.io/rss", {
   limit: 3,
   template: "<div class='feed-container'><div class='feed-entries'>{entry}<div class='feed-entry'><div class='feed-date'>{date}</div><div class='feed-title'><a href='{url}'>{title}</div></a><div class='feed-author'>By <span class='author-text'>{author}</span></div><div class='feed-body'>{shortBody}</div></div>{/entry}</div></div>",
 
