16e1c2fef030d585937869399c64788d6a8a37fe
Orjan Wirdheim <orjan@plotagon.com>
2017-02-21 16:31:43 +0100

    Fixed stickySearch with algolia

--- resources/javascript/stickySearch.js
+++ resources/javascript/stickySearch.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   $(window).scroll(function () {
     if ($(window).scrollTop() > 150) {
-      $('#stickySearch').addClass('navbar-fixed');
+      $('#stickySearch').addClass('stickyFixed');
+      $('#stickyFill').addClass('stickyFill');
     }
     if ($(window).scrollTop() < 151) {
-      $('#stickySearch').removeClass('navbar-fixed');
+      $('#stickySearch').removeClass('stickyFixed');
+      $('#stickyFill').removeClass('stickyFill');
     }
   });
 });
