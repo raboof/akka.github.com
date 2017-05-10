f0d776b46db71d288cbeaf1bc4a308b44761dd64
Johan Andrén <johan@markatta.com>
2017-05-10 14:06:59 +0200

    Search box working #389

---
+++ resources/javascript/docs-search.js
@@ -0,0 +1,41 @@
+---
+# front matter to make jekyll process it
+layout: null
+---
+
+$(function () {
+
+    // set by jekyll
+    var latestAkkaVersion = "{{ page.current_akka_version }}";
+
+    // initalizes doc search if there is a search box
+    // depends on external algolia js being loaded
+    var searchRef = $("#search-ref-docs");
+    if (searchRef.length > 0) {
+        $("#docs-language").change(function (el) {
+            searchRef.find("li.lang").addClass("hidden");
+            searchRef.find("li#" + el.currentTarget.value.toLowerCase()).removeClass("hidden");
+        });
+
+        docsearch({
+            apiKey: '543bad5ad786495d9ccd445ed34ed082',
+            indexName: 'akka_io',
+            inputSelector: '#search-scala',
+            algoliaOptions: {
+                hitsPerPage: 5,
+                facetFilters: '[' + '["language:scala","language:general"]' + ',"version:' + latestAkkaVersion + '"]'
+            }
+        });
+
+        docsearch({
+            apiKey: '543bad5ad786495d9ccd445ed34ed082',
+            indexName: 'akka_io',
+            inputSelector: '#search-java',
+            algoliaOptions: {
+                hitsPerPage: 5,
+                facetFilters: '[' + '["language:java","language:general"]' + ',"version:' + latestAkkaVersion + '"]'
+            }
+        });
+    }
+
+});
